fca00b1bf973ed1eba4f7858691de676f76ed7b8
juannestradacerritos-lang <juannestradacerritos@gmail.com>
2026-09-19 18:11:46 -0600

    Add service worker for offline caching

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'moto-tarifa-offline-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Devuelve la copia guardada sin internet, o la descarga si tienes datos
+        return response || fetch(event.request);
+      })
+  );
+});
